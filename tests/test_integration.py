900ac7763d6c5e3bc3e99f0032ed59ba46bb5d93
Cade Ekblad-Frank <cade@e-f.me>
2020-10-20 16:20:03 -0500

    A little lint cleanup

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,12 +5,14 @@ import cade_task.cli
 
 TASK_COMMANDS = ["list", "lists", "add", "complete", "sync", "open"]
 
+
 @pytest.fixture
 def cli():
     """
     Returns an instance of click.testing.CliRunner
     """
     return CliRunner()
+
 
 def test_no_command(cli):
     """
@@ -23,6 +25,7 @@ def test_no_command(cli):
     for c in TASK_COMMANDS:
         assert c in result.output
 
+
 @pytest.mark.parametrize("command", TASK_COMMANDS)
 def test_subcommands_exist(cli, command):
     """
@@ -31,8 +34,12 @@ def test_subcommands_exist(cli, command):
     result = cli.invoke(cade_task.cli.main, [command, "--help"])
     assert result.exit_code == 0
 
+
 def test_subcommand_list(cli):
     unknown_list = "uhduh34852f56"
     unknown_list_result = cli.invoke(cade_task.cli.list, ["--list", unknown_list])
-    assert unknown_list_result.exit_code is not 0
-    assert "No reminders list matching {}".format(unknown_list) in unknown_list_result.output
+    assert unknown_list_result.exit_code != 0
+    assert (
+        "No reminders list matching {}".format(unknown_list)
+        in unknown_list_result.output
+    )
